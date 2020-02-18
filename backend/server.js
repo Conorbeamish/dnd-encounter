dc3375b199ac695b19eee1ffe97e2476440ad602
conorbeamish <conorbeamish@hotmail.co.uk>
2020-02-18 20:37:09 +0000

    Add proxy and concorrently

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express       = require("express"),
-      bodyParser    = require("body-parser");
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port, ()=> {
-    console.log(`Server running on: ${port}`);
-});
