639ccb1f03d9844e7546ba7ce8a19db855e6d8ad
conorbeamish <conorbeamish@hotmail.co.uk>
2020-03-08 13:49:06 +0000

    Add homepage

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Homepage.css"
+
+const Homepage = () => (
+    <div className="homepage">
+        <h1>DnD Encounter</h1>
+        <Link to="/signup">
+            Sign up Here
+        </Link>
+    </div>
+)
+
+export default Homepage;
